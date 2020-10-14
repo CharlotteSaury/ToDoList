ce2123688b034246f99cacf72350a46ee657fd22
CharlotteSaury <saury.charlotte@wanadoo.fr>
2020-10-14 15:39:52 +0200

    add task validity entity tests

---
+++ tests/Entity/TaskTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Tests\Entity;
+
+use App\Entity\Task;
+use App\Tests\Utils\AssertHasErrors;
+use Liip\TestFixturesBundle\Test\FixturesTrait;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class TaskTest extends KernelTestCase
+{
+    use AssertHasErrors;
+    use FixturesTrait;
+
+    /**
+     * Assert valid entity is valid.
+     *
+     * @return void
+     */
+    public function testValidEntity()
+    {
+        $task = new Task();
+        $task->setTitle('valid title');
+        $task->setContent('valid content');
+        $this->assertHasErrors($task, 0);
+    }
+
+    /**
+     * Assert invalid entity (email, company) in invalid.
+     *
+     * @return void
+     */
+    public function testInvalidEntity()
+    {
+        $invalidTask = new Task();
+        $invalidTask->setTitle('');
+        $invalidTask->setContent('');
+        $this->assertHasErrors($invalidTask, 2);
+    }
+}
